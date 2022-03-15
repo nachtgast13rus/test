5924168769a4576b7297c8ffc39222ba69b39ac6
devHome <dtygushev@yandex.ru>
2022-03-15 22:09:20 +0300

    add homework

--- stepic/matrix.py
+++ stepic/matrix.py
@@ -1,3 +1,6 @@
+import copy
+
+
 def create_matrix():
     new_list = []
     while True:
@@ -9,6 +12,11 @@ def create_matrix():
     return new_list
 
 
+def create_empty_matrix(matrix):
+    empty_matrix = copy.deepcopy(matrix)
+    return empty_matrix
+
+
 def print_matrix(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
@@ -16,20 +24,22 @@ def print_matrix(matrix):
         print()
 
 
-def change_matrix(matrix):
+def change_matrix(matrix, empty_matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             if i == 0 and j == 0:
-                matrix[i][j] = matrix[i][j + 1] + matrix[i][j - 1] + matrix[i + 1][j] + matrix[i - 1][j]
+                empty_matrix[i][j] = matrix[i][j + 1] + matrix[i][j - 1] + matrix[i + 1][j] + matrix[i - 1][j]
             if i == 0 and j == len(matrix[i]) - 1:
-                matrix[i][j] = matrix[i][j - 1] + matrix[i + 1][j] + matrix[i][j - j] + matrix[i - 1][j]
-#TODO сделать чтобы мвссив не менялся при расчете
+                empty_matrix[i][j] = matrix[i][j - 1] + matrix[i + 1][j] + matrix[i][j - j] + matrix[i - 1][j]
 
+
+# TODO continue...
 
 def main():
     my_matrix = [[9, 5, 3], [0, 7, -1], [-5, 2, 9]]  # create_matrix()
-    change_matrix(my_matrix)
-    print_matrix(my_matrix)
+    empty_matrix = create_empty_matrix(my_matrix)
+    change_matrix(my_matrix, empty_matrix)
+    print_matrix(empty_matrix)
 
 
 if __name__ == "__main__":
