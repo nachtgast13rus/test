db656de8d2da8f9a5ea967e512042baf46724ee5
nachtgast13rus <dtygushev@yandex.ru>
2022-04-08 16:30:39 +0300

    add

--- hyperskill/probe.py
+++ hyperskill/probe.py
@@ -1,1 +1,27 @@
-print(eval(input()))
+# class Robot:
+#     def __init__(self, name, variety):
+#         self.name = name
+#         self.variety = variety
+#         print("Robot")
+#
+#     def get_info(self):
+#         return "{} is a {} robot".format(self.name, self.variety)
+#
+#
+# class ServiceRobot(Robot):
+#     def __init__(self, name):
+#         super().__init__(name, 'service')
+#
+#
+# chappi = ServiceRobot("Chappi")
+# print(chappi.get_info())
+class NegativeSumError(Exception):
+    def __init__(self):
+        super().__init__()
+
+
+def sum_with_exceptions(a, b):
+    if (a + b) < 0:
+        raise NegativeSumError
+    else:
+        print(a + b)
